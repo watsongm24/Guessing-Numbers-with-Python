acca01d15ce84f14de66052695ab635bdecabae9
Watson Marcelain <30502965+watsongm24@users.noreply.github.com>
2023-09-03 21:44:47 -0400

    Number Guessing Game using Python

---
+++ Guessing.py
@@ -0,0 +1,14 @@
+import random
+
+n = random.randrange(1, 12)
+guess = int(input("Input the gessing number: "))
+while n != guess:
+    if guess < n:
+        print("Too Low")
+        guess = int(input("Enter the number again: "))
+    elif guess > n:
+        print("Too high")
+        guess = int(input("Enter the number again: "))
+    else:
+        break
+print("You got it right")
